b6ffbe72e877ef6a914cb983d37787369ce73b6c
Vitor Izidoro <vitorizidoro288@gmail.com>
2026-04-24 21:09:56 -0300

    CommitFinal

--- MusicProject/src/App.jsx
+++ MusicProject/src/App.jsx
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 export default function App() {
+  const [started, setStarted] = useState(false);
+
   return (
     <div className="hero">
-      <div className="overlay">
-        <h1>Essa é pra você</h1>
-        <p>
-          Cada nota dessa música carrega um pedaço do que eu sinto…
-        </p>
+      <div className="particles"></div>
 
-        <audio controls className="player">
-          <source src="/musica.mp3" type="audio/mpeg" />
-        </audio>
+      {!started ? (
+        <div className="intro">
+          <h1>Eu fiz isso pra você</h1>
+          <button onClick={() => setStarted(true)}>
+            O que tem aqui… é só pra você!!
+          </button>
+        </div>
+      ) : (
+        <div className="content">
+          <h1>Essa música é sua</h1>
+          <p>
+           Cada detalhe dessa música foi pensado em você…
+em tudo que eu sinto, mas nem sempre consigo dizer.
+          </p>
 
-        
-      </div>
+          <audio controls autoPlay className="player">
+            <source src="/musica.mp3" type="audio/mpeg" />
+          </audio>
+
+          <span className="assinatura">com amor ❤️</span>
+        </div>
+      )}
     </div>
   );
 }
